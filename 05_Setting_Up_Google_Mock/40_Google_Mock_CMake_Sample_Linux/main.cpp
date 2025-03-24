3114bcc35972b88a557e4148636c20619733bca0
Andrey Maksimov <andrey.maksimov@gmail.com>
2025-03-24 15:16:40 +0300

    Chapter 40  sample updated

--- 05_Setting_Up_Google_Mock/40_Google_Mock_CMake_Sample_Linux/main.cpp
+++ 05_Setting_Up_Google_Mock/40_Google_Mock_CMake_Sample_Linux/main.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+class SomeClass
+{
+    public:
+    SomeClass() = default;
+    virtual void SomeMethod() {
+        std::cout << "SomeMethod" << std::endl;
+    }
+};
+
+class MockedClass : public SomeClass
+{
+    public:
+    MockedClass() = default;
+
+    // here we use new syntax :
+    // https://github.com/google/googletest/blob/main/docs/gmock_cook_book.md#creating-mock-classes
+    MOCK_METHOD(void, SomeMethod, (), (override));
+};
 
 int add(int a, int b)
 {
     return a + b;
+}
+
+TEST(TestSample, TestMock) {
+    MockedClass mc;
+    EXPECT_CALL(mc, SomeMethod()).Times(1);
+    mc.SomeMethod();
 }
 
 TEST(TestSample, TestAddition)
